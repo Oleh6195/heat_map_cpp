5874adc26eaea94cd31c3096c0441c6c946e5cc2
Oleh <tyzhai.o@ucu.edu.ua>
2019-06-08 18:36:40 +0300

    matrix 1000x1000

--- matrix_maker.py
+++ matrix_maker.py
@@ -1,13 +1,16 @@
 import random
 with open("matrix.txt", "w") as f:
-    for i in range(20):
-        for j in range(20):
-            if i < 5:
-                f.write(str(random.randint(15,25)) + ", ")
-            elif i < 10:
-                f.write(str(random.randint(25, 40)) + ", ")
-            elif i < 15:
-                f.write(str(random.randint(60, 80)) + ", ")
+    for i in range(1000):
+        for j in range(1000):
+            if i == 0 or j ==0 or i==999 or j == 999:
+                f.write(str(50) + ",")
+            elif i < 200:
+                f.write(str(random.randint(15,25)) + ",")
+            elif i < 400:
+                f.write(str(random.randint(25, 40)) + ",")
+            elif i < 700:
+                f.write(str(random.randint(60, 80)) + ",")
             else:
-                f.write(str(random.randint(80, 100)) + ", ")
+                f.write(str(random.randint(80, 100)) + ",")
+
         f.write("\n")
